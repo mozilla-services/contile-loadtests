01e7a662eaff70365b3dc6d99016f136fee7d8d9
Ethan Donowitz <edonowitz@mozilla.com>
2021-05-25 16:54:40 +0000

    feat: Create molotov load tests
    Closes #1

--- README.md
+++ README.md
@@ -14,16 +14,25 @@ $ pip install -r requirements.txt
 
 ## Usage:
 
+The following environment variables must be set:
+* `TIMEOUT`: The timeout (in seconds) for the request made to Contile
+* `TARGET_URL`: The URL of the endpoint to be load tested
+* `TEST_LOCATION_HEADER_NAME`: The of the HTTP header used to manually specify the location from which the request originated. This should match the value of CONTILE_LOCATION_TEST_HEADER on Contile
+
 The load tests were written using
 [Molotov](https://molotov.readthedocs.io/en/stable/) and can be started using
 the following command:
 
 ```sh
-$ molotov -c -v d <duration>
+$ molotov -c -v d <duration> -s <scenario name>
 ```
 
 where `<duration>` is the length of time in seconds that you want the load test
-to run.
+to run, and `<scenario name>` is one of the following:
+* `request_from_consistent_location_with_consistent_user_agent`: Requests all originate from the same location (USCA) and have the same user agent (Google Chrome on Windows)
+* `request_from_random_location_with_consistent_user_agent`: Requests originate from randomly-chosen locations but have the same user agent (Google Chrome on Windows)
+* `request_from_consistent_location_with_random_user_agent`: Requests originate from the same location but have randomly-chosen user agents
+* `request_from_random_location_with_random_user_agent`: Requests originate from a randomly-chosen location and have randomly-chosen user agents
 
 Check the Molotov documentation for details on other options available to run
 the tests.
---
+++ loadtest.py
@@ -0,0 +1,87 @@
+from molotov import scenario
+from aiohttp import ClientTimeout
+import os
+import random
+import xml.etree.ElementTree as ET
+
+CLDR_SUBDIVISION_FILENAME = 'unicode_cldr_subdivision_codes.xml'
+TARGET_URL = os.environ['TARGET_URL']
+TEST_LOCATION_HEADER_NAME = os.environ['TEST_LOCATION_HEADER_NAME']
+TEST_USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393',
+    'Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
+    'Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-G570Y Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/4.0 Chrome/44.0.2403.133 Mobile Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko'
+]
+TIMEOUT = float(os.environ['TIMEOUT'])
+
+
+def parse_subdivision_codes_file():
+    tree = ET.parse(CLDR_SUBDIVISION_FILENAME)
+    root = tree.getroot()
+    subgroups = root[1]
+
+    return subgroups
+
+
+TEST_LOCATIONS = parse_subdivision_codes_file()
+
+
+@scenario()
+async def request_from_consistent_location_with_consistent_user_agent(session):
+    headers = {
+        'User-Agent': TEST_USER_AGENTS[0], TEST_LOCATION_HEADER_NAME: 'US, USCA'}
+    timeout = ClientTimeout(total=TIMEOUT)
+
+    async with session.get(TARGET_URL, headers=headers, timeout=timeout) as resp:
+        assert resp.status == 200
+
+
+@scenario()
+async def request_from_random_location_with_consistent_user_agent(session):
+    headers = {'User-Agent': TEST_USER_AGENTS[0],
+               TEST_LOCATION_HEADER_NAME: get_random_location()}
+    timeout = ClientTimeout(total=TIMEOUT)
+
+    async with session.get(TARGET_URL, headers=headers, timeout=timeout) as resp:
+        assert resp.status == 200
+
+
+@scenario()
+async def request_from_consistent_location_with_random_user_agent(session):
+    headers = {'User-Agent': get_random_user_agent(),
+               TEST_LOCATION_HEADER_NAME: 'US, USCA'}
+    timeout = ClientTimeout(total=TIMEOUT)
+
+    async with session.get(TARGET_URL, headers=headers, timeout=timeout) as resp:
+        assert resp.status == 200
+
+
+@scenario()
+async def request_from_random_location_with_random_user_agent(session):
+    headers = {'User-Agent': get_random_user_agent(),
+               TEST_LOCATION_HEADER_NAME: get_random_location()}
+    timeout = ClientTimeout(total=TIMEOUT)
+
+    async with session.get(TARGET_URL, headers=headers, timeout=timeout) as resp:
+        assert resp.status == 200
+
+
+def get_random_location():
+    subgroup = random.choice(TEST_LOCATIONS)
+    attributes = subgroup.attrib
+    code = attributes['type']
+    subdivisions = attributes['contains'].split(' ')
+    subdivision = random.choice(subdivisions)
+
+    return f'{code}, {subdivision.upper()}'
+
+
+def get_random_user_agent():
+    return random.choice(TEST_USER_AGENTS)
