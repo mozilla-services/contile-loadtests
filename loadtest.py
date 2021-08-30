9a42a1153d9ea185e889f26a20ef3260527bdeeb
Ankita Shrivastava <ashrivastava@mozilla.com>
2021-08-30 15:54:44 -0400

    Update the user-agents

--- loadtest.py
+++ loadtest.py
@@ -9,24 +9,22 @@ _TARGET_URL = os.environ.get('TARGET_URL', 'http://localhost:8000/v1/tiles')
 _TEST_LOCATION_HEADER_NAME = os.environ.get(
     'TEST_LOCATION_HEADER_NAME', 'X-Test-Location')
 _TEST_FIREFOX_USER_AGENTS = [
-    'Mozilla/5.0 (Windows NT 10.0; rv:10.0) Gecko/20100101 Firefox/10.0',
+    'Mozilla/5.0 (Windows NT 10.0; rv:10.0) Gecko/20100101 Firefox/91.0',
 
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:10.0) Gecko/20100101 '
-    'Firefox/10.0',
+    'Firefox/91.0',
 
-    'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0',
-    'Mozilla/5.0 (Android; Mobile; rv:40.0) Gecko/40.0 Firefox/40.0',
-    'Mozilla/5.0 (Android; Tablet; rv:40.0) Gecko/40.0 Firefox/40.0',
-    'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
-    'Mozilla/5.0 (Android 4.4; Tablet; rv:41.0) Gecko/41.0 Firefox/41.0',
+    'Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/91.0',
+    'Mozilla/5.0 (Android 11; Mobile; rv:92.0) Gecko/92.0 Firefox/92.0',
+    'Mozilla/5.0 (Android; Tablet; rv:92.0) Gecko/92.0 Firefox/92.0',
 
     'Mozilla/5.0 (iPod touch; CPU iPhone OS 8_3 like Mac OS X) '
     'AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 '
     'Safari/600.1.4',
 
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) '
-    'AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 '
-    'Mobile/12F69 Safari/600.1.4',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 11_5_1 like Mac OS X) '
+    'AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/35.0 Mobile/15E148 '
+    'Safari/605.1.15',
 
     'Mozilla/5.0 (iPad; CPU iPhone OS 8_3 like Mac OS X) '
     'AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 '
